06f3ad7a339a04bc3793d4d28e6c6cffc069236c
inhooo00 <chldlsgh0987@naver.com>
2025-09-01 09:28:47 +0900

    Feature(#3): Diary Entity 생성

---
+++ src/main/kotlin/shop/maeum/domain/diary/domain/Diary.kt
@@ -0,0 +1,39 @@
+import jakarta.persistence.*
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.Where
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import shop.maeum.domain.diary.domain.PrivacySetting
+import shop.maeum.global.entity.BaseEntity
+import shop.maeum.global.entity.Status
+
+@Entity
+@SQLDelete(sql = "UPDATE diary SET status = 'UN_ACTIVE' WHERE diary_id = ?")
+@Where(clause = "status = 'ACTIVE'")
+@EntityListeners(AuditingEntityListener::class)
+class Diary (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "diary_id", nullable = false)
+    val id: Long? = null,
+
+    @Column(nullable = false)
+    val title: String,
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    val content: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "privacy_setting", nullable = false)
+    val privacySetting: PrivacySetting,
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    val feedback: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    var status: Status = Status.ACTIVE,
+
+//    @Column(name = "member_id", nullable = false)
+//    val memberId: Long
+) : BaseEntity()
