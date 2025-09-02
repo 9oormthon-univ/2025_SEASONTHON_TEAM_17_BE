2ad765239e31895a4ca480ccbfee7efb4956c266
inhooo00 <chldlsgh0987@naver.com>
2025-09-02 15:25:08 +0900

    Modify(#8): 환경변수 경로 수정

--- src/main/kotlin/shop/maeum/domain/security/config/CorsConfig.kt
+++ src/main/kotlin/shop/maeum/domain/security/config/CorsConfig.kt
@@ -1,15 +1,14 @@
 package shop.maeum.domain.security.config
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConfigurationProperties(prefix = "cors")
-class CorsConfig(
-    var allowedHeaders: List<String> = listOf("*"),
-    var allowedMethods: List<String> = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS"),
-) {
-    @Value("\${cors.allowed-origins}")
-    lateinit var allowedOrigins: List<String>
+@ConfigurationProperties(prefix = "spring.security.cors")
+class CorsConfig {
+    var allowedOrigins: List<String> = emptyList()
+    var allowedPaths: List<String> = emptyList()
+    var allowedHeaders: List<String> = listOf("*")
+    var allowedMethods: List<String> = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
 }
+
