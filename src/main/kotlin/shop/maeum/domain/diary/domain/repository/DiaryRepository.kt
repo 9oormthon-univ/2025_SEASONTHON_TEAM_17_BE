38e86ed6bbd56b09b22e59fb485bd4907cc06584
inhooo00 <chldlsgh0987@naver.com>
2025-09-03 09:26:02 +0900

    - 특정 년도와 달을 파라미터로 요청하면 그 달에 내가 쓴 일기 유무를 반환 - 특정 년도와 달과 일을 파라미터로 요청하면 그 날에 일기 쓴 내역을 조회 - 내가 오늘 날짜에 작성했는지 유무를 반환 - swagger를 위한 docs 파일 작성

--- src/main/kotlin/shop/maeum/domain/diary/domain/repository/DiaryRepository.kt
+++ src/main/kotlin/shop/maeum/domain/diary/domain/repository/DiaryRepository.kt
@@ -4,10 +4,23 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import shop.maeum.domain.diary.domain.Diary
+import java.time.LocalDateTime
 
 interface DiaryRepository : JpaRepository<Diary, Long> {
     fun findAllByMemberIdOrderByCreatedAtDesc(
         memberId: String, pageable: Pageable
     ): Page<Diary>
+
+    fun findAllByMemberIdAndCreatedAtBetween(
+        memberId: String, createdAt: LocalDateTime, createdAt2: LocalDateTime
+    ): List<Diary>
+
+    fun findFirstByMemberIdAndCreatedAtBetween(
+        memberId: String, createdAt: LocalDateTime, createdAt2: LocalDateTime
+    ): Diary?
+
+    fun existsByMemberIdAndCreatedAtBetween(
+        memberId: String, createdAt: LocalDateTime, createdAt2: LocalDateTime
+    ): Boolean
 
 }
